35aa5481aaceb076583416e9c99ab133c541898a
Michał Wolny <michal.wolny@slowhop.com>
2025-02-15 21:59:38 +0100

    Refactor docker_utils lib

--- commands/Legacy/update_rates.sh
+++ commands/Legacy/update_rates.sh
@@ -4,11 +4,18 @@ COMMAND_NAME="ExchangeRates:Update"
 
 run_command() {
     echo "${GREEN}[Commander] Uruchamianie aktualizacji kursów walut...${NO_COLOR}"
-    currencies=("pln" "eur" "czk" "usd" "huf" "chf" "gbp" "uah" "dkk" "nok" "sek")
-    for currency in "${currencies[@]}"; do
-        docker exec sh-legacy-legacy-1 bin/console slowhop:pricing:currency:update-exchange-rates "$currency" -e docker --pretty &
-    done
-    wait
-    echo "${GREEN}[Commander] Aktualizacja zakończona${NO_COLOR}"
+
+    CONTAINER_ID=$(get_container_id)
+    if [ -z "$CONTAINER_ID" ]; then
+        echo "${RED}Nie znaleziono kontenera legacy.${NO_COLOR}"
+    else
+        currencies=("pln" "eur" "czk" "usd" "huf" "chf" "gbp" "uah" "dkk" "nok" "sek")
+        for currency in "${currencies[@]}"; do
+            docker exec "$CONTAINER_ID" bin/console slowhop:pricing:currency:update-exchange-rates "$currency" -e docker --pretty &
+        done
+        wait
+        echo "${GREEN}[Commander] Aktualizacja zakończona${NO_COLOR}"
+    fi
+
     read -p "Naciśnij Enter, aby kontynuować..."
 }
--- commands/Legacy/update_translations.sh
+++ commands/Legacy/update_translations.sh
@@ -5,13 +5,13 @@ COMMAND_NAME="Translations:Update"
 run_command() {
     echo "${GREEN}[Commander] Uruchamianie aktualizacji tłumaczeń...${NO_COLOR}"
 
-    CONTAINER_ID=$(docker ps --filter "name=sh-legacy-legacy" --format "{{.ID}}")
+    CONTAINER_ID=$(get_container_id)
     if [ -z "$CONTAINER_ID" ]; then
         echo "${RED}Nie znaleziono kontenera legacy.${NO_COLOR}"
-        exit 1
+    else
+        docker exec "$CONTAINER_ID" bin/console slowhop:system:translations:refresh -e docker
+        echo "${GREEN}[Commander] Aktualizacja tłumaczeń zakończona.${NO_COLOR}"
     fi
 
-    docker exec "$CONTAINER_ID" bin/console slowhop:system:translations:refresh -e docker
-    echo "${GREEN}[Commander] Aktualizacja tłumaczeń zakończona.${NO_COLOR}"
     read -p "Naciśnij Enter, aby kontynuować..."
 }
