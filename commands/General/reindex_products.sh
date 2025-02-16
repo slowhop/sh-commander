16faf07657efb147079eb27e9026b55440ba8ad3
Michał Wolny <michal.wolny@slowhop.com>
2025-02-16 14:19:14 +0100

    Display info about products to reindex count.

--- commands/General/reindex_products.sh
+++ commands/General/reindex_products.sh
@@ -3,25 +3,26 @@
 COMMAND_NAME="Reindex:Products"
 
 update_processed_count() {
-    local new_count=$(grep -o "ProductToIndexMessage was handled successfully" "$logs_file" | wc -l)
+    local new_count=$(grep -o "ProductToIndexMessage was handled successfully" "$indexer_logs_file" | wc -l)
 
     if [ "$new_count" -ne "$processed_count" ]; then
         tput sc
-        printf "${GREEN}[Commander] Przetworzono produktów: %d${NO_COLOR}\r" "$new_count"
+        printf "${YELLOW}[Commander] ${GREEN}Przetworzono produktów:${NO_COLOR} %d\r" "$new_count"
         tput rc
         processed_count=$new_count
     fi
 }
 
 run_command() {
-    echo "${GREEN}[Commander] Uruchamianie reindeksacji produktów...${NO_COLOR}"
+    echo "${YELLOW}[Commander] ${GREEN}Uruchamianie reindeksacji produktów...${NO_COLOR}"
 
     local can_run_script=true
-    local logs_file="$logs_dir/reindex_products.log"
+    local indexer_logs_file="$logs_dir/reindex_products_indexer.log"
+    local legacy_logs_file="$logs_dir/reindex_products_legacy.log"
     local processed_count=0
 
-    # Usunięcie pliku z logami (jeśli istnieje)
-    rm -f "$logs_file"
+    # Usunięcie plików z logami (jeśli istnieją)
+    rm -f "$indexer_logs_file" "$legacy_logs_file"
 
     # Sprawdzenie istnienia kontenera legacy
     local legacy_container_id=$(get_legacy_container_id)
@@ -41,21 +42,40 @@ run_command() {
     if [ "$can_run_script" = true ]; then
         # Czyszczenie indeksu w kontenerze indexer
         docker exec "$index_container_id" bin/console slowhop:index:recreate -i product_accommodation -e docker
-        echo "${GREEN}[Commander] Indeks wyczyszczony w kontenerze indexer.${NO_COLOR}"
+        echo "${YELLOW}[Commander] ${GREEN}Indeks wyczyszczony w kontenerze indexer.${NO_COLOR}"
 
-        # Uruchomienie komendy reindeksacji w kontenerze legacy
-        docker exec "$legacy_container_id" php -d memory_limit=1G bin/console slowhop:products:reindex -f -e docker -n &
+        # Uruchomienie komendy reindeksacji w kontenerze legacy (logowanie do pliku)
+        docker exec "$legacy_container_id" php -d memory_limit=1G bin/console slowhop:products:reindex -f -e docker -n > "$legacy_logs_file" 2>&1 &
         local legacy_pid=$!
-        echo "${GREEN}[Commander] Reindeksacja produktów uruchomiona w kontenerze legacy.${NO_COLOR}"
+        echo "${YELLOW}[Commander] ${GREEN}Reindeksacja produktów uruchomiona w kontenerze legacy.${NO_COLOR}"
 
         # Utworzenie folderu logs, jeśli nie istnieje
         mkdir -p "$logs_dir"
 
-        # Uruchomienie komendy w kontenerze indexer i przekierowanie wyjścia do pliku w folderze logs
-        docker exec "$index_container_id" bin/console messenger:consume --env=staging -- product > "$logs_file" 2>&1 &
+        # Uruchomienie komendy w kontenerze indexer i przekierowanie wyjścia do pliku
+        docker exec "$index_container_id" bin/console messenger:consume --env=staging -- product > "$indexer_logs_file" 2>&1 &
         local indexer_pid=$!
-        echo "${GREEN}[Commander] Konsumowanie wiadomości uruchomione w kontenerze indexer.${NO_COLOR}"
+        echo "${YELLOW}[Commander] ${GREEN}Konsumowanie wiadomości uruchomione w kontenerze indexer.${NO_COLOR}"
         echo ""
+
+        # Oczekiwanie na pierwszy log w legacy
+        echo "${YELLOW}[Commander] ${GREEN}Oczekiwanie na informacje o liczbie produktów do reindeksacji...${NO_COLOR}"
+        while [ ! -s "$legacy_logs_file" ]; do
+            sleep 1
+        done
+
+        # Analiza pliku legacy w poszukiwaniu liczby produktów
+        local total_products
+        while read -r line; do
+            echo "$line" | grep -q "\[Command\] [0-9]\+ products to reindex" && {
+                total_products=$(echo "$line" | grep -o "[0-9]\+ products to reindex" | grep -o "[0-9]\+")
+                echo "${YELLOW}[Commander] ${GREEN}Ilość produktów do przetworzenia:${NO_COLOR} $total_products"
+                break
+            }
+
+            # Jeśli nie pasuje do wzorca, wyświetlamy w konsoli
+            echo "$line"
+        done < "$legacy_logs_file"
 
         # Czekanie na zakończenie procesu reindeksacji
         while kill -0 $legacy_pid 2> /dev/null; do
@@ -66,29 +86,29 @@ run_command() {
         wait $legacy_pid
 
         # Monitorowanie wiadomości konsumpcji w kontenerze indexer
-        echo "${GREEN}[Commander] Monitorowanie konsumpcji wiadomości w kontenerze indexer przez 10 sekund...${NO_COLOR}"
+        echo "${YELLOW}[Commander] ${GREEN}Monitorowanie konsumpcji wiadomości w kontenerze indexer przez 10 sekund...${NO_COLOR}"
         local end_time=$((SECONDS + 10))
         while [ $SECONDS -lt $end_time ]; do
             update_processed_count
-            if grep -q "ProductToIndexMessage" "$logs_file"; then
-                echo "${GREEN}[Commander] Wykryto nowe wiadomości. Resetowanie czasu oczekiwania.${NO_COLOR}"
+            if grep -q "ProductToIndexMessage" "$indexer_logs_file"; then
+                echo "${YELLOW}[Commander] ${GREEN}Wykryto nowe wiadomości. Resetowanie czasu oczekiwania.${NO_COLOR}"
                 end_time=$((SECONDS + 10))
-                > "$logs_file"  # Czyszczenie pliku logów
+                > "$indexer_logs_file"  # Czyszczenie pliku logów
             fi
             sleep 1
         done
 
-        echo "${GREEN}[Commander] Konsumowanie wiadomości zakończone.${NO_COLOR}"
+        echo "${YELLOW}[Commander] ${GREEN}Konsumowanie wiadomości zakończone.${NO_COLOR}"
 
         # Zakończenie procesu indexera, jeśli jeszcze istnieje
         if kill -0 $indexer_pid 2> /dev/null; then
             kill $indexer_pid
-            echo "${GREEN}[Commander] Proces indexera został zakończony.${NO_COLOR}"
+            echo "${YELLOW}[Commander] ${GREEN}Proces indexera został zakończony.${NO_COLOR}"
         fi
 
-        # Usunięcie pliku z logami
-        rm -f "$logs_file"
-        echo "${GREEN}[Commander] Plik z logami został usunięty.${NO_COLOR}"
+        # Usunięcie plików z logami
+        rm -f "$indexer_logs_file" "$legacy_logs_file"
+        echo "${YELLOW}[Commander] ${GREEN}Pliki z logami zostały usunięte.${NO_COLOR}"
     fi
 
     read -p "Naciśnij Enter, aby kontynuować..."
