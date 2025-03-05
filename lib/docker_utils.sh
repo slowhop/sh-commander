6d8a6c31b5e0e3096e18eef4e4f43524575dbe93
Michał Wolny <michal.wolny@slowhop.com>
2025-03-05 09:08:17 +0100

    Use patterns to find docker containers.

--- lib/docker_utils.sh
+++ lib/docker_utils.sh
@@ -1,21 +1,23 @@
 get_container_id() {
-    local container_name="$1"
-    CONTAINER_ID=$(docker ps --filter "name=${container_name}" --format "{{.ID}}")
+    local container_pattern="$1"
+    CONTAINER_ID=$(docker ps --filter "name=${container_pattern}" --format "{{.ID}}")
+
     if [ -z "$CONTAINER_ID" ]; then
         return 1
     fi
+
     echo "$CONTAINER_ID"
     return 0
 }
 
 get_legacy_container_id() {
-    get_container_id "sh-legacy-legacy"
+    get_container_id "sh[-_]legacy[-_]legacy"
 }
 
 get_redis_container_id() {
-    get_container_id "sh-legacy-redis-1"
+    get_container_id "sh[-_]legacy[-_]redis[-_]1"
 }
 
 get_indexer_container_id() {
-    get_container_id "sh-legacy-indexer"
+    get_container_id "sh[-_]legacy[-_]indexer"
 }
